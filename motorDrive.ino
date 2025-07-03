1099eaa4650eefc0b2c6958450245256c53be496
curtissimo37 <157269695+curtissimo37@users.noreply.github.com>
2025-07-03 11:10:38 -0400

    Update posServo motorDrive.ino
    Update posServo function to meet requirements of workshop 3.

--- motorDrive.ino
+++ motorDrive.ino
@@ -16,19 +16,10 @@ void setup() {
   myservo2.attach(9); //configure digital pin 9 to control the servo
 }
 
-void posServo(){
-  //Delete the example code and put your code for part 1 (a) here
-
-  for(int pos = 0; pos <= 180; pos += 1) // goes from 0 degrees to 180 degrees 
-  {                                  // in steps of 1 degree 
-    myservo1.write(pos);              // tell servo to go to position in variable 'pos' 
-    delay(15);                       // waits 15ms for the servo to reach the position 
-  } 
-  for(int pos = 180; pos>=0; pos-=1)     // goes from 180 degrees to 0 degrees 
-  {                                
-    myservo1.write(pos);              // tell servo to go to position in variable 'pos' 
-    delay(15);                       // waits 15ms for the servo to reach the position 
-  } 
+void posServo(int pos){
+  // Constrain pos so that it must always be between 0 and 180
+    pos = constrain(pos,0,180);  
+    myservo1.write(pos); // tell servo to go to position in variable 'pos'  
 }
 
 void rotServo(){
